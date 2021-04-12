1cf479e83e072a48ec205f327efb5036053a2f8b
Jessie Lou Balintongog <79207479+JessieLou14@users.noreply.github.com>
2021-04-12 08:25:29 +0800

    Add files via upload

---
+++ arrays.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main ()
+{
+    int element[8],e, array, sum=0;//declaration of array
+
+    printf("enter size of array: ", array);
+    scanf("%d", &array);
+    //conditional statements
+    if (array>11)
+    {
+        printf("Array size limit is 5 to 10 only.");
+        return 0;
+    }
+    if (array<4)
+    {
+        printf("Array size limit is 5 to 10 only.");
+        return 0;
+    }
+    printf("____________________________________\n\n");
+    printf("size of array: %d\n", array);
+    //traversal array
+    for(e=0;e<array;++e)
+    {
+        printf("element %d: ", e+1);
+        scanf("%d", &element[e]);
+        //adding the elements created by the user
+        sum += element[e];
+    }
+    printf("Sum of all array elements: %d\n\n\n", sum);
+
+    return 0;
+    }
